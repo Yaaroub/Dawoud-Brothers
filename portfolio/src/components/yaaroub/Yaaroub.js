470c77dacf66f4b9e94510012cd29fd9bcef75b8
yaaroub <yarobdaoud1997@gmail.com>
2022-12-24 13:13:31 +0100

    my info

--- portfolio/src/components/yaaroub/Yaaroub.js
+++ portfolio/src/components/yaaroub/Yaaroub.js
@@ -1,9 +1,23 @@
-import YaaroubsBild from "./AvatarMaker.png"
-import "./Yaaroub.css"
+import { useState } from "react";
+import YaaroubsBild from "./AvatarMaker.png";
+import "./Yaaroub.css";
 export default function Yaaroub() {
+  const [data, setData] = useState(false);
+
+  const clickhandler = () => {
+    setData(!data);
+  };
   return (
-    <div className="yaaroub" >
-      <img src={YaaroubsBild}  alt="Bild" />
+    <div className="yaaroub">
+      <img onClick={clickhandler} src={YaaroubsBild} alt="Bild" />
+      {data ? (
+        <>
+          {" "}
+          <h1>Yaaroub Al Haj Dawoud</h1>
+          <h2>WEB DEVELOPER</h2>
+          <h2>26 Jahre alt</h2>
+        </>
+      ) : null}
     </div>
-  )
+  );
 }
